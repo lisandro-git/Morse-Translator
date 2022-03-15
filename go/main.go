0e27956426c1e8a27fbbc63dd6e1670eb506768c
Edode <git-lisandro@x394.org>
2022-03-15 17:30:47 -0400

    Added user input and beautified the code

--- go/main.go
+++ go/main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"morse-translator/morse"
+	"os"
 	"strings"
+	"morse-translator/morse"
 )
 
-func main()  {
-	var x []string = morse.Encode(strings.ToLower("Hello world!"), morse.All_signs)
-	fmt.Println(x)
-	var y string = morse.Decode(x, morse.All_signs)
-	fmt.Println(y)
+func from_text() {
+	fmt.Printf("Plaintext -> ")
+	in := bufio.NewReader(os.Stdin)
+	
+	line, err := in.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	strings.TrimSuffix(line, "\n")
+	var encoded []string = morse.Encode(line, morse.All_signs)
+	fmt.Println(encoded)
+}
+
+func from_morse() {
+	fmt.Printf("Morse code -> ")
+	in := bufio.NewReader(os.Stdin)
+	
+	line, err := in.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	line = strings.TrimSuffix(line, "\n")
+	morse_arr := strings.Split(line, " ")
+	var decoded string = morse.Decode(morse_arr, morse.All_signs)
+	fmt.Println(decoded)
+}
+
+func main() {
+	from_text()
+	from_morse()
 }
--- go/morse/morse_to_word.go
+++ go/morse/morse_to_word.go
@@ -3,11 +3,22 @@ package morse
 func Decode(morse_code []string, morse_signs map[string]string) string {
 	var result string
 	var i int
+	var space bool // edode : used to add a space if there is two spaces in between words
 	
 	for i = 0; i < len(morse_code); i++ {
 		for key, value := range morse_signs {
-			if (value == morse_code[i]){
+			if morse_code[i] == "" {
+				if space {
+					result += " "
+					break;
+				}
+				space = true
+				continue;
+			}
+			if (value == morse_code[i]) {
 				result += key
+				space = false
+				break;
 			}
 		}
 	}
