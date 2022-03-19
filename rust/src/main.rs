8012fd425ea05c79ec774be1d3f45af221043c7e
Edode <git-lisandro@x394.org>
2022-03-19 11:39:23 -0400

    Cleaned up files

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,7 @@ pub mod morse;
 
 
 fn main() {
-        let encoded = morse::word_to_morse::encode(String::from("Hello how are you ?"));
+    let encoded = morse::word_to_morse::encode(String::from("Hello how are you ?"));
     println!("{}", encoded);
     let decoded = morse::morse_to_word::decode(encoded);
     println!("{}", decoded);
